442eba826c42923f0c26102e15294c13885a27eb
Anatolii Ohorodnyk <a.ohorodnyk@playtini.ua>
2025-01-23 14:37:18 +0000

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,18 +37,19 @@ echo "INPUT_GIT_REPOSITORY_URL: ${INPUT_GIT_REPOSITORY_URL}"
 echo "INPUT_GIT_SHA: ${INPUT_GIT_SHA}"
 echo "INPUT_GIT_BRANCH: ${INPUT_GIT_BRANCH}"
 
-ls -lah
+if [ -n "${INPUT_GIT_BRANCH}" ]; then
+    echo "checkout begin ${INPUT_GIT_BRANCH}"
+    git checkout "${INPUT_GIT_BRANCH}"
+    echo "checkout end ${INPUT_GIT_BRANCH}"
+fi
+
 if [ -n "${GIT_TAG}" ]; then
     echo "checkout begin ${GIT_TAG}"
     git checkout "${GIT_TAG}"
     echo "checkout end ${GIT_TAG}"
 fi
 
-if [ -n "${INPUT_GIT_BRANCH}" ]; then
-    echo "checkout begin ${INPUT_GIT_BRANCH}"
-    git checkout "${INPUT_GIT_BRANCH}"
-    echo "checkout end ${INPUT_GIT_BRANCH}"
-fi
+ls -lah
 
 set -- "-t" "${IMAGE_NAME}:${IMAGE_TAG}" \
   "--label" "org.label-schema.schema-version=1.0" \
